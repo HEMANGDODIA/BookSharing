3aee7d9e9af872d09fbb7fefd587f120f1066183
Jatin231 <61380369+Jatin231@users.noreply.github.com>
2020-02-26 03:40:27 +0530

    hello21

---
+++ app/src/main/java/com/BookSharing/Short_notesSenior.java
@@ -0,0 +1,14 @@
+package com.BookSharing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Short_notesSenior extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_short_notes_senior);
+    }
+}
