7486f1333686346ad40f2007039775995a07d3d2
Jatin231 <61380369+Jatin231@users.noreply.github.com>
2020-02-26 04:51:18 +0530

    hello21

--- app/src/main/java/com/BookSharing/Short_notesSenior.java
+++ app/src/main/java/com/BookSharing/Short_notesSenior.java
@@ -9,6 +9,6 @@ public class Short_notesSenior extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_short_notes_senior);
+        setContentView(R.layout.activity_notes_senior);
     }
 }
